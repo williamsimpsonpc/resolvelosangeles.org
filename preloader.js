94cff14718c6558b4abc350f9a1300be44cb5120
williamsimpsonpc <williamsimpsonpc@gmail.com>
2022-04-24 15:15:38 -0700

    Cleanup

---
+++ preloader.js
@@ -0,0 +1,18 @@
+const preloader = document.querySelector('.preloader');
+
+const fadeEffect = setInterval(() => {
+  // if we don't set opacity 1 in CSS, then
+  // it will be equaled to "" -- that's why
+  // we check it, and if so, set opacity to 1
+  if (!preloader.style.opacity) {
+    preloader.style.opacity = 1;
+  }
+  if (preloader.style.opacity > 0) {
+    preloader.style.opacity -= 1;
+  } else {
+    clearInterval(fadeEffect);
+    preloader.style.display="none"
+  }
+}, 1500);
+      
+window.addEventListener('load');
