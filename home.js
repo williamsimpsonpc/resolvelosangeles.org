7afb112442073c07153a6676ba9ca99d6dbeb31d
Minater247 <nreed132@gmail.com>
2023-10-24 20:11:10 -0700

    Initial formatter pass, minor fixes

--- home.js
+++ home.js
@@ -1,13 +1,13 @@
-function callDateandTime(){
-    var currentDate=(new Date()).toLocaleDateString();
-    var currentTime=(new Date()).toLocaleTimeString();
-    document.getElementById('big').innerHTML=`${currentDate}  ${currentTime} Los Angeles`;
-  }
-setInterval(function(){  callDateandTime() }, 1000);
+function callDateandTime() {
+    var currentDate = (new Date()).toLocaleDateString();
+    var currentTime = (new Date()).toLocaleTimeString();
+    document.getElementById('big').innerHTML = `${currentDate}  ${currentTime} Los Angeles`;
+}
+setInterval(function () { callDateandTime() }, 1000);
 
-function callDateTime(){
-    var currentDate=(new Date()).toLocaleDateString();
-    var currentTime=(new Date()).toLocaleTimeString();
-    document.getElementById('small').innerHTML=`${currentDate}  ${currentTime} LA`;
-  }
-setInterval(function(){  callDateTime() }, 1000);
+function callDateTime() {
+    var currentDate = (new Date()).toLocaleDateString();
+    var currentTime = (new Date()).toLocaleTimeString();
+    document.getElementById('small').innerHTML = `${currentDate}  ${currentTime} LA`;
+}
+setInterval(function () { callDateTime() }, 1000);
--- scripts.js
+++ scripts.js
@@ -11,99 +11,99 @@
 const preloader = document.querySelector('.preloader');
 
 const fadeEffect = setInterval(() => {
-  // if we don't set opacity 1 in CSS, then
-  // it will be equaled to "" -- that's why
-  // we check it, and if so, set opacity to 1
-  if (!preloader.style.opacity) {
-    preloader.style.opacity = 1;
-  }
-  if (preloader.style.opacity > 0) {
-    preloader.style.opacity -= 1;
-  } else {
-    clearInterval(fadeEffect);
-    preloader.style.display="none"
-  }
+    // if we don't set opacity 1 in CSS, then
+    // it will be equaled to "" -- that's why
+    // we check it, and if so, set opacity to 1
+    if (!preloader.style.opacity) {
+        preloader.style.opacity = 1;
+    }
+    if (preloader.style.opacity > 0) {
+        preloader.style.opacity -= 1;
+    } else {
+        clearInterval(fadeEffect);
+        preloader.style.display = "none"
+    }
 }, 1500);
 
-            
+
 window.addEventListener('load');
 
 
-(function() {
-  // Init
-  var container = document.getElementById("container"),
-    inner = document.getElementById("inner");
+(function () {
+    // Init
+    var container = document.getElementById("container"),
+        inner = document.getElementById("inner");
 
-  // Mouse
-  var mouse = {
-    _x: 0,
-    _y: 0,
-    x: 0,
-    y: 0,
-    updatePosition: function(event) {
-      var e = event || window.event;
-      this.x = e.clientX - this._x;
-      this.y = (e.clientY - this._y) * -1;
-    },
-    setOrigin: function(e) {
-      this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
-      this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
-    },
-    show: function() {
-      return "(" + this.x + ", " + this.y + ")";
-    }
-  };
+    // Mouse
+    var mouse = {
+        _x: 0,
+        _y: 0,
+        x: 0,
+        y: 0,
+        updatePosition: function (event) {
+            var e = event || window.event;
+            this.x = e.clientX - this._x;
+            this.y = (e.clientY - this._y) * -1;
+        },
+        setOrigin: function (e) {
+            this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+            this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+        },
+        show: function () {
+            return "(" + this.x + ", " + this.y + ")";
+        }
+    };
 
-  // Track the mouse position relative to the center of the container.
-  mouse.setOrigin(container);
+    // Track the mouse position relative to the center of the container.
+    mouse.setOrigin(container);
 
-  //-----------------------------------------
+    //-----------------------------------------
 
-  var counter = 0;
-  var updateRate = 10;
-  var isTimeToUpdate = function() {
-    return counter++ % updateRate === 0;
-  };
+    var counter = 0;
+    var updateRate = 10;
+    var isTimeToUpdate = function () {
+        return counter++ % updateRate === 0;
+    };
 
-  //-----------------------------------------
+    //-----------------------------------------
 
-  var onMouseEnterHandler = function(event) {
-    update(event);
-  };
+    var onMouseEnterHandler = function (event) {
+        update(event);
+    };
 
-  var onMouseLeaveHandler = function() {
-    inner.style = "";
-  };
+    var onMouseLeaveHandler = function () {
+        inner.style = "";
+    };
 
-  var onMouseMoveHandler = function(event) {
-    if (isTimeToUpdate()) {
-      update(event);
-    }
-  };
+    var onMouseMoveHandler = function (event) {
+        if (isTimeToUpdate()) {
+            update(event);
+        }
+    };
 
-  //-----------------------------------------
+    //-----------------------------------------
 
-  var update = function(event) {
-    mouse.updatePosition(event);
-    updateTransformStyle(
-      (mouse.y / inner.offsetHeight / 2).toFixed(2),
-      (mouse.x / inner.offsetWidth / 2).toFixed(2)
-    );
-  };
+    var update = function (event) {
+        mouse.updatePosition(event);
+        updateTransformStyle(
+            (mouse.y / inner.offsetHeight / 2).toFixed(2),
+            (mouse.x / inner.offsetWidth / 2).toFixed(2)
+        );
+    };
 
-  var updateTransformStyle = function(x, y) {
-    var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
-    inner.style.transform = style;
-    inner.style.webkitTransform = style;
-    inner.style.mozTransform = style;
-    inner.style.msTransform = style;
-    inner.style.oTransform = style;
-  };
+    var updateTransformStyle = function (x, y) {
+        var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+        inner.style.transform = style;
+        inner.style.webkitTransform = style;
+        inner.style.mozTransform = style;
+        inner.style.msTransform = style;
+        inner.style.oTransform = style;
+    };
 
-  //-----------------------------------------
+    //-----------------------------------------
 
-  container.onmouseenter = onMouseEnterHandler;
-  container.onmouseleave = onMouseLeaveHandler;
-  container.onmousemove = onMouseMoveHandler;
+    container.onmouseenter = onMouseEnterHandler;
+    container.onmouseleave = onMouseLeaveHandler;
+    container.onmousemove = onMouseMoveHandler;
 })();
 
